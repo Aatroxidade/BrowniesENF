24efcb559da12550b9a81f1819e63ab7ddd864fc
Rodrigo <rodrigodcco46@gmail.com>
2026-05-27 17:10:28 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,26 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+const admin = require("firebase-admin");
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+const serviceAccount = require("./serviceAccountKey.json");
 
-const firebaseConfig = {
+admin.initializeApp({
 
-  apiKey: "AIzaSyAUnUndHUcUsEeKT4ScS7vcroyFHJSBzFI",
+  credential: admin.credential.cert(serviceAccount)
 
-  authDomain: "cookies-site.firebaseapp.com",
+});
 
-  projectId: "cookies-site",
+const db = admin.firestore();
 
-  storageBucket: "cookies-site.firebasestorage.app",
-
-  messagingSenderId: "736821689099",
-
-  appId: "1:736821689099:web:2ea4cbe4287139b4f9400e",
-
-  measurementId: "G-7EC2BG8QRR"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export { app, db };
+module.exports = db;
