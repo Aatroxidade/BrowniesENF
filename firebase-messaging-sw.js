a1e367f6498ac0542b7d95ae7db7f03b40594120
Rodrigo <rodrigodcco46@gmail.com>
2026-06-22 15:20:48 -0300

    Temos brownie!

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,3 +30,30 @@ appId:
 
 const messaging =
 firebase.messaging();
+
+messaging.onBackgroundMessage(
+
+(payload)=>{
+
+self.registration.showNotification(
+
+payload.notification.title,
+
+{
+
+body:
+payload.notification.body,
+
+icon:
+"/imgs/logo.png",
+
+badge:
+"/imgs/logo.png"
+
+}
+
+);
+
+}
+
+);
