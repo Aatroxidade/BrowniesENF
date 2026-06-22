409eec77404fd42f6cb3a63aca195dc7be8c8e17
Rodrigo <rodrigodcco46@gmail.com>
2026-06-22 14:19:25 -0300

    Arrumando notif

--- jss/notificacao.js
+++ jss/notificacao.js
@@ -1,18 +1,43 @@
-import { app } from "./firebase.js";
+import {
+app,
+db,
+messaging
+}
+from "./firebase.js";
 
 import {
-getMessaging,
 getToken
 }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
-const messaging =
-getMessaging(app);
+import {
+doc,
+setDoc
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 
 async function ativarNotificacao() {
 
 try {
 
+// usuário já bloqueou anteriormente
+if (
+Notification.permission ===
+"denied"
+) {
+
+alert(
+"Você bloqueou as notificações. Clique no cadeado ao lado do site → Notificações → Permitir."
+);
+
+return;
+
+}
+
+// pedir permissão
 const permissao =
 await Notification.requestPermission();
 
@@ -28,6 +53,7 @@ return;
 
 }
 
+// gerar token
 const token =
 await getToken(
 messaging,
@@ -42,12 +68,42 @@ console.log(
 token
 );
 
-// depois salvaremos no firestore
+// salvar firestore
+await setDoc(
+
+doc(
+db,
+"notificacoes",
+token
+),
+
+{
+
+token,
+
+criadoEm:
+new Date()
 
 }
+
+);
+
+console.log(
+"TOKEN SALVO"
+);
+
+alert(
+"Notificações ativadas com sucesso!"
+);
+
+}
+
 catch(err){
 
-console.log(err);
+console.log(
+"ERRO:",
+err
+);
 
 }
 
